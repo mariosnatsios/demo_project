142afe71ebf821b57a0d5b2c6c5fe3686ffa4a6a
MariosNatsios <m.natsios@schoox.com>
2024-02-28 13:01:36 +0200

    Another check

--- src/tests/test_check_logout.py
+++ src/tests/test_check_logout.py
@@ -6,7 +6,9 @@ class Test_Check_Logout(fixtures.Fixtures):
         Checks successful logout after a valid login
     """
     def test_1_check_logout(self):
-        """
+        """     
+            *******************************
              Perform valid login then hit the logout button and validate the redirection to the initial login page.
+             *******************************
         """
         check_logout()
